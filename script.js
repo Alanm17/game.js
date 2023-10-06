304a1ed6b0626ca89b864835c36b7ba86f4b4c54
Alanm17 <mukhammadazizkholdarov@gmail.com>
2023-10-06 21:24:55 +0900

    game finished

--- script.js
+++ script.js
@@ -11,17 +11,34 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+// const btnNew = document.querySelector('.btn--new')
 // console.log(diceEl);
 ////////////////////
-score0El.textContent = 0;
-score1El.textContent = 0;
-/////////////////////
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+
+let scores, currentScore, activePlayer, playing;
+// let btnNew = false;
 // diceEl.classList.remove('btn');
-diceEl.classList.add('hidden');
+// diceEl.classList.add('hidden');
+const init = function () {
+  /////////////////////
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('.player--winner');
+  player1El.classList.remove('.player--winner');
+  player0El.classList.remove('.player--active');
+  player1El.classList.remove('.player--active');
+};
+init();
+//  current0El.classList.add('hidden')
 
 // diceEl.classList.add('.hidden');
 ////////////////////
@@ -64,7 +81,7 @@ btnHold.addEventListener('click', function () {
       //finish the game
       playing = false;
       diceEl.classList.add('hidden');
-      console.log(diceEl);
+      // console.log(diceEl);
 
       document
         .querySelector(`.player--${activePlayer}`)
@@ -77,3 +94,4 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+btnNew.addEventListener('click', init);
